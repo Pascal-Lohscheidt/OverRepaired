e41bb6b0384b5d449a47904f3106e0b78f01cb78
Maikon Farias <maikonfarias@gmail.com>
2020-01-31 23:07:49 +0100

    Breakable Object and Issue Manager and other classes

---
+++ Assets/Scripts/InGameEventManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameEventManager : Singleton<InGameEventManager>
+{
+    static Random random = new Random();
+
+    static int maxNumberOfEvents = 3;
+
+    static float timeBetweenEvents = 30f;
+    float timeUntilNextEvent = 0f;
+
+    List<BreakableObject> breakableObjects = new List<BreakableObject>();
+
+
+    public void Register(BreakableObject breakableObject){
+        breakableObjects.Add(breakableObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeUntilNextEvent -= Time.deltaTime;
+
+        if(timeUntilNextEvent < 0 && GetNumberOfEvents() < maxNumberOfEvents)
+        {
+            CreateRandomEvent();
+        }
+    }
+
+    int GetNumberOfEvents()
+    {
+        int numberOfEvents = 0;
+        foreach(var breakableObject in breakableObjects)
+        {
+            if(breakableObject.IsBroken())
+            {
+                numberOfEvents++;
+            }
+        }
+        return numberOfEvents;
+    }
+
+    int GetNumberOfWorkingObjects()
+    {
+        int numberOfObjects = 0;
+        foreach(var breakableObject in breakableObjects)
+        {
+            if(!breakableObject.IsBroken())
+            {
+                numberOfObjects++;
+            }
+        }
+        return numberOfObjects;
+    }
+
+    void CreateRandomEvent()
+    {
+        if(GetNumberOfWorkingObjects > 0){
+
+            // get random item from the list
+            int i = random.Next(breakableObjects.Count);
+            var breakableObject = breakableObjects[i];
+            
+            if(breakableObject.IsBroken()){
+                breakableObject.Break();
+            }
+            else
+            {
+                // previous object was already broken, find another one
+                CreateRandomEvent();
+            }
+
+        }
+    }
+}
---
+++ Assets/Scripts/IssueManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IssueManager : MonoBehaviour
+{
+
+    BreakableObject breakableObject;
+
+    List<GameObject> components;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void HandleBehaviour()
+    {
+
+    }
+
+    void Fix()
+    {
+        this.breakableObject.Fix();
+    }
+}
