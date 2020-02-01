5df047098ad178fffcbeb2194d4cf9bb73c1be33
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2020-02-01 09:47:38 +0100

    added Coloring to the breakable Object

--- Assets/Scripts/BreakableObject.cs
+++ Assets/Scripts/BreakableObject.cs
@@ -6,20 +6,25 @@ public class BreakableObject : MonoBehaviour
 {
     public string objectName;
     public bool working = true;
+    private new Renderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
         InGameEventManager.Instance.Register(this);
+        renderer = GetComponent<Renderer>();
+        FixObject();
     }
 
     public void FixObject()
     {
+        renderer.material.color = Color.green;
         working = true;
     }
 
     public void BreakObject()
     {
+        renderer.material.color = Color.red;
         IssueManager.Instance.CreateIssue(this); //Creating a new Issue because this component Broke
         working = false;
     }
