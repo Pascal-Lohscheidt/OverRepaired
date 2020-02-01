50aaddb766dc98b97df2c27eb5dab6a107b3fef9
gianni_ <dreck@posteo.de>
2020-02-01 11:54:06 +0100

    Create Spawner

---
+++ Assets/Scripts/ComponentSpawner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        IssueManager.Instance.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
