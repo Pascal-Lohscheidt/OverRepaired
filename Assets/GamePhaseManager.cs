fa1a45a90da312796f114d1a0505ee19b3a7a7ca
Pascal Lohscheidt <pascal.lohscheidt@gmail.com>
2020-02-02 12:48:05 +0100

    added menu scene

--- Assets/GamePhaseManager.cs
+++ Assets/GamePhaseManager.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GamePhaseManager : MonoBehaviour
 {
 
+    public void LoseGame()
+    {
 
+    }
  
+
+    public void ExitGameToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +26,6 @@ public class GamePhaseManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)) ExitGameToMenu();
     }
 }
